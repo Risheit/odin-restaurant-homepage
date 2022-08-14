fa08855407e31b140d13024b6a4b2c1b009255aa
Risheit <80658104+Risheit@users.noreply.github.com>
2022-08-14 11:17:54 -0500

    Initialize repository and webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = requre('path');
+const HtmlWebpackPlugin = requre('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js'
+    },
+    devtool: 'inline-source-map',
+    devserver: {
+        static: './dist',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Development',
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    optimatization: {
+        runtimeChunk: 'single',
+    },
+}
