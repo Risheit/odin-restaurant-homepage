2df73ec49213f8a2d731cc7ea9b4ee7911b8a516
Risheit <80658104+Risheit@users.noreply.github.com>
2022-08-14 22:26:51 -0500

    Change element class names

--- src/home.js
+++ src/home.js
@@ -3,11 +3,11 @@ export default root => {
 
     const h1 = document.createElement("h1");
     h1.textContent = "Dummy Restaraunt";
-    h1.classList.add("home-h1");
+    h1.classList.add("home");
 
     const h2 = document.createElement("h2");
     h2.textContent = "Grab a bite to eat.";
-    h2.classList.add("home-h2");
+    h2.classList.add("home");
 
     root.appendChild(h1);
     root.appendChild(h2);
--- src/menu.js
+++ src/menu.js
@@ -3,11 +3,11 @@ export default root => {
 
     const h1 = document.createElement("h1");
     h1.textContent = "Menu";
-    h1.classList.add("home-h1");
+    h1.classList.add("menu");
 
     const h2 = document.createElement("h2");
     h2.textContent = "Lot's of things available!";
-    h2.classList.add("home-h2");
+    h2.classList.add("menu");
 
     root.appendChild(h1);
     root.appendChild(h2);
