ed53d6ecfbd3db88dda5e2ddcec531c62406fc5e
Risheit <80658104+Risheit@users.noreply.github.com>
2022-08-14 22:31:56 -0500

    Add sidebar content switchers

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 import "./index.css";
+import setHomeContent from "./home.js";
+import setMenuContent from "./menu.js";
+import setContactContent from "./contact.js"
 
 initBody();
 
+
 function initBody() {
     const sidebar = initSidebar();
+    const main = initMain();
+
+    sidebar.querySelector("#home")
+        .addEventListener("change", () => setHomeContent(main));
+    sidebar.querySelector("#menu")
+        .addEventListener("change", () => setMenuContent(main));
+    sidebar.querySelector("#contact")
+        .addEventListener("change", () => setContactContent(main));
 
     document.body.appendChild(sidebar);
+    document.body.appendChild(main);
+
+    setHomeContent(main);
 }
 
 function initSidebar() {
-
     const options = document.createElement("form");
     options.classList.add("sidebar");
 
@@ -29,7 +43,6 @@ function initSidebar() {
 }
 
 function initSidebarItem(id, content, imgSrc, imgAlt) {
-    
     const img = document.createElement("img");
     img.classList.add("sidebar-image");
     img.src = imgSrc;
@@ -57,4 +70,10 @@ function initSidebarItem(id, content, imgSrc, imgAlt) {
     item.appendChild(label);
 
     return item;
+}
+
+function initMain() {  
+    const main = document.createElement("main");
+    
+    return main
 }
